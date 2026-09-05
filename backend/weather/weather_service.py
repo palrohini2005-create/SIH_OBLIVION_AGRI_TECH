aa7e53614436397605c78ca4b18d204cdd2abf3d
Zaina <gangopadhyayzaina123@gmail.com>
2026-09-05 12:30:48 +0530

    Add weather service

---
+++ backend/weather/weather_service.py
@@ -0,0 +1,27 @@
+import requests
+
+
+def get_weather(latitude, longitude):
+    url = "https://api.open-meteo.com/v1/forecast"
+
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "current": "temperature_2m,relative_humidity_2m,rain",
+        "hourly": "precipitation_probability",
+        "forecast_days": 1
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code != 200:
+        return {"error": "Unable to fetch weather data"}
+
+    data = response.json()
+
+    return {
+        "temperature": data["current"]["temperature_2m"],
+        "humidity": data["current"]["relative_humidity_2m"],
+        "rainfall": data["current"]["rain"],
+        "rain_probability": data["hourly"]["precipitation_probability"][0]
+    }
